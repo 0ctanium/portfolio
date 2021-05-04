227a0544bbadb55996c23c37ff5b3a3b8e611421
Benjamin Lepas <mail@octanium.fr>
2021-05-05 00:48:18 +0200

    Fixed sitemap generation on Vercel

--- pages/api/generateSitemap/common.ts
+++ pages/api/generateSitemap/common.ts
@@ -7,7 +7,17 @@ const generateCommonSitemap: NextApiHandler = async (req, res) => {
     fs.unlinkSync('./public/sitemap.xml');
   } catch (e) {}
 
-  exec('next-sitemap', (err) => {
+  require('@next/env').loadEnvConfig(
+    process.cwd(),
+    process.env.NODE_ENV === 'development'
+  );
+
+  // Load cli
+  // console.log(require('@octanium/next-sitemap/dist/cjs/cli'));
+
+  res.status(203).json({ success: true });
+
+  exec(`node node_modules/@octanium/next-sitemap/dist/cjs/cli.js`, (err) => {
     if (err) {
       throw err;
     } else {
