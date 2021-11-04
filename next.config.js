1c611a61c50b352bf322fa7d1a824c9b8f78729b
Octanium <octaniumcontact@gmail.com>
2021-11-04 15:58:37 +0100

    :sparkles:  Added PWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 const { i18n } = require('./next-i18next.config');
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
-module.exports = {
+module.exports = withPWA({
   i18n,
-};
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/]
+  }
+});
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,20 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="stylesheet" href="https://use.typekit.net/cri2dli.css" />
+
+          <link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' />
+          <link rel='apple-touch-icon' sizes='152x152' href='/icons/touch-icon-ipad.png' />
+          <link rel='apple-touch-icon' sizes='180x180' href='/icons/touch-icon-iphone-retina.png' />
+          <link rel='apple-touch-icon' sizes='167x167' href='/icons/touch-icon-ipad-retina.png' />
+
+
+          <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
+
+
+          <link rel='manifest' href='/manifest.json' />
+          <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#000' />
+          <link rel='shortcut icon' href='/favicon.ico' />
         </Head>
         <body
           className={clsx(
